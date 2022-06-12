b13f62ca1a5b963f096419f5fcec6883a2c8a184
Elizabeth Bringas <bringas.liza@gmail.com>
2022-06-12 00:05:06 -0400

    Agrega nombres

---
+++ src/components/Listadonombres.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import uniqid from "uniqid";
+
+const ListadoNombres = () =>{
+
+    const [nombre, setNombre] = useState('');
+    const [listaNombres, setListaNombres] = useState([]);
+
+    const addNombre= (e)=>{
+        e.preventDefault();
+        const nuevoNombre={
+            id:uniqid(),
+            tituloNombre:nombre
+        }
+        setListaNombres([...listaNombres, nuevoNombre]);
+    }
+
+
+    return(
+        <div>
+            <h2>Aplicacion CRUD BASICO</h2>
+            <div className="row">
+                <div className="col">
+                    <h2>Listado de nombres</h2>
+                </div>
+                <div className="col">
+                    <h2>Formulario para agregar nombres</h2>
+                    <form onSubmit={(e)=> addNombre(e)} className="form-group">
+                        <input onChange={(e)=>{setNombre(e.target.value)}}
+                               className="form-control mb-3" 
+                               type="text" 
+                               placeholder="Introduce el nombre"/>
+
+                        <input className="btn btn-primary btn-block"
+                               type="submit" 
+                               value="Registrar nombre"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ListadoNombres;
