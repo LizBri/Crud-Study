a5599abace8fa8d188d3d36e87ba845a00663a9e
Elizabeth Bringas <bringas.liza@gmail.com>
2022-06-18 19:28:10 -0400

    Correccion Obj Destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ListadoNombres from './components/Listadonombres';
 import Bienvenida from './components/Bienvenida';
+import Cards from './components/Cards';
 
 function App() {
   return (
@@ -11,9 +12,25 @@ function App() {
       <Bienvenida nombre="Emma"/>
       <Bienvenida nombre="Nicolas"/>
       <Bienvenida nombre="Thor"/>
-
+      <hr/>
       <br/>
       <ListadoNombres/>
+      <hr/>
+
+      <h3> Propiedades de Componentes</h3>
+      <br/>
+      <div className='row'>
+        <div className='col'>
+          <Cards/>
+
+        </div>
+
+      </div>
+
+
+
+
+
     </div>
   );
 }
